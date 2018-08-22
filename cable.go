dff74c24df6edba2311fe25e8925804b8427ee09
Jahn <ioannis.kiwe@gmail.com>
2018-08-22 13:37:54 +0200

    Init

---
+++ cable.go
@@ -0,0 +1,61 @@
+package cable
+
+import (
+	"sync"
+	"time"
+)
+
+// Throttle returns a function that no matter how many times it is invoked,
+// it will only execute once within the specified interval
+func Throttle(f func(), interval time.Duration) func() {
+	last := time.Now()
+	return func() {
+		now := time.Now()
+		if now.Sub(last) > interval {
+			last = now
+			f()
+		}
+	}
+}
+
+// Debounce returns a function that no matter how many times it is invoked,
+// it will be invoked only after a specified interval has passed from its last invocation
+func Debounce(f func(), interval time.Duration) func() {
+	cancel := func() {}
+	return func() {
+		cancel()
+		cancel = SetTimeout(f, interval)
+	}
+}
+
+// SetTimeout postpones the execution of f for a specified interval
+func SetTimeout(f func(), interval time.Duration) func() {
+	var isCanceled bool
+	var access sync.Mutex
+	go (func() {
+		time.Sleep(interval)
+		access.Lock()
+		defer access.Unlock()
+		if !isCanceled {
+			f()
+		}
+	})()
+
+	cancel := func() {
+		access.Lock()
+		isCanceled = true
+		access.Unlock()
+	}
+	return cancel
+}
+
+// SetInterval executes repeatedly the specified function with the specified interval
+// When f returns false the loop will break
+func SetInterval(f func() bool, interval time.Duration) {
+	for _ = range time.Tick(interval) {
+		shouldContinue := f()
+		if !shouldContinue {
+			break
+		}
+	}
+}
---
+++ cable_test.go
@@ -0,0 +1,108 @@
+package cable_test
+
+import (
+	"cable"
+	"testing"
+	"time"
+)
+
+func Test_SetTimeout(t *testing.T) {
+	timeoutInterval1 := 100 * time.Millisecond
+
+	calledAt := time.Now()
+	cable.SetTimeout(func() {
+		executedAt := time.Now()
+		delta := executedAt.Sub(calledAt)
+		if delta <= timeoutInterval1 {
+			t.Errorf("SetTimeout callback was called earlier: %d, want >: %d.", delta, timeoutInterval1)
+		}
+
+	}, timeoutInterval1)
+	time.Sleep(200 * time.Millisecond)
+
+	timeoutInterval2 := 50 * time.Millisecond
+	isCanceled := true
+	cancel := cable.SetTimeout(func() {
+		isCanceled = false
+	}, timeoutInterval2)
+
+	cancel()
+	time.Sleep(100 * time.Millisecond)
+
+	if !isCanceled {
+		t.Errorf("SetTimeout cancel callback execution failed")
+	}
+}
+
+func Test_SetInterval(t *testing.T) {
+	interval := 20 * time.Millisecond
+	maxInvocations := 5
+	var invocation int
+	startedAt := time.Now()
+	cable.SetInterval(func() bool {
+		invocation++
+		if invocation == maxInvocations {
+			return false
+		}
+		return true
+	}, interval)
+
+	endedAt := time.Now()
+
+	delta := endedAt.Sub(startedAt)
+	if delta < 100 {
+		t.Errorf("SetInterval ")
+	}
+}
+
+func Test_Throttle(t *testing.T) {
+	throttleInterval := 30 * time.Millisecond
+	executionInterval := 5 * time.Millisecond
+	totalExecutionInterval := 200 * time.Millisecond
+	var timesInvoked int
+
+	throttledFunc := cable.Throttle(func() {
+		timesInvoked++
+	}, throttleInterval)
+
+	startedAt := time.Now()
+	cable.SetInterval(func() bool {
+		delta := time.Now().Sub(startedAt)
+		throttledFunc()
+		if delta > totalExecutionInterval {
+			return false
+		}
+		return true
+	}, executionInterval)
+
+	maxExpectedInvocations := 6
+	if timesInvoked != maxExpectedInvocations {
+		t.Errorf("Throttled callback has not been invoked the expected amount of times: %d, want: %d.", timesInvoked, maxExpectedInvocations)
+
+	}
+}
+
+func Test_Debounce(t *testing.T) {
+	debounceInterval := 30 * time.Millisecond
+	executionInterval := 5 * time.Millisecond
+	totalExecutionInterval := 200 * time.Millisecond
+	var timesInvoked int
+
+	maxExpectedInvocations := 1
+	debouncedFunc := cable.Debounce(func() {
+		timesInvoked++
+		if timesInvoked != maxExpectedInvocations {
+			t.Errorf("Debounce callback has not been invoked the expected amount of times: %d, want: %d.", timesInvoked, maxExpectedInvocations)
+		}
+	}, debounceInterval)
+
+	startedAt := time.Now()
+	cable.SetInterval(func() bool {
+		delta := time.Now().Sub(startedAt)
+		debouncedFunc()
+		if delta > totalExecutionInterval {
+			return false
+		}
+		return true
+	}, executionInterval)
+}
